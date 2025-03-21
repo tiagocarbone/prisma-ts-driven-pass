fae4dc56f3aa55d598f20c85937c398be4829f71
Tiago Carbone <carbonetiago@id.uff.br>
2025-03-21 07:11:21 -0300

    req params isPositive verification

--- src/services/credentials-service.ts
+++ src/services/credentials-service.ts
@@ -19,17 +19,25 @@ export async function getCredentialsService(userId: number){
 
 export async function getCredentialByIdService(userId: number, credentialId: number){
 
+
+    if(credentialId < 0) throw { type: "bad request", message: "parâmetro negativo" };
+
     const credential = await credentialsGetByIdRepository(userId, credentialId);
     return credential
 }
 
 export async function deleteCredentialByIdService(userId: number, credentialId: number){
 
+    if(credentialId < 0) throw { type: "bad request", message: "parâmetro negativo" };
+
     await credentialsDeleteByIdRepository(userId, credentialId);
     
 }
 
 export async function putCredentialService(userId: number, credentialId: number, credential: PutCredential){
+
+    if(credentialId < 0) throw { type: "bad request", message: "parâmetro negativo" };
+
 
     await credentialPutRepository(userId, credentialId, credential);
     
